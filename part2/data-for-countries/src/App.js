67a86489196a61c2cb15021b1f9ae708d2980a22
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-09 14:09:56 +0800

    2.20 done

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,42 +1,60 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const weather_api_key = process.env.REACT_APP_WEATHER_API_KEY
+
 const Details = ({ prop }) => {
-  return (
-    <div>
-      <h1>{prop[0].name.common}</h1>
-      <p>capital {prop[0].capital}</p>
-      <p>area {prop[0].area}</p>
-      <h2>languages:</h2>
-      <ul>
-        {Object.values(prop[0].languages).length > 1 ?
-          Object.values(prop[0].languages).map(language => <li>{language}</li>)
-          : <li>{Object.values(prop[0].languages)[0]}</li>}
-      </ul>
-      <img src={prop[0].flags.png} alt={`${prop[0].name.common} flag`} />
-    </div >
-  )
+  const [isLoading, setIsLoading] = useState(true);
+  const [weather, setWeather] = useState([]);
+  const country = prop[0]
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${weather_api_key}&units=metric`).then((response) => {
+      console.log(response)
+      setWeather(response)
+      setIsLoading(false)
+    })
+  },[])
+
+  if (!isLoading) {
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+        <h2>languages:</h2>
+        <ul>
+          {Object.values(country.languages).length > 1 ?
+            Object.values(country.languages).map(language => <li>{language}</li>)
+            : <li>{Object.values(country.languages)[0]}</li>}
+        </ul>
+        <img src={country.flags.png} alt={`${country.name.common} flag`} />
+        <h2>Weather in {country.name.common}</h2>
+        <p>temperatrue {weather.data.main.temp} Celcius</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`} alt={`weather in ${country.name.common}`} />
+        <p>wind {weather.data.wind.speed} m/s</p>
+      </div>
+    )
+  }
+
 }
 
 
-
 const Search = ({ searchResult, chooseOne }) => {
-
-
   if (searchResult.length === 1) {
     return (
       <Details prop={searchResult} />
     )
   }
 
-  return (searchResult.map(result => <p key={result.name.common}>{result.name.common} <button onClick={()=>chooseOne(result.name.common)}>show</button></p>))
-
+  return (searchResult.map(result => <p key={result.name.common}>{result.name.common} <button onClick={() => chooseOne(result.name.common)}>show</button></p>))
 }
-const App = () => {
 
+const App = () => {
   const [countries, setCountries] = useState([])
   const [searchTarget, setSearchTarget] = useState("")
   const [searchResult, setSearchResult] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const chooseOne = (name) => {
     const choosen = searchResult.filter(result => result.name.common === name)
@@ -48,6 +66,7 @@ const App = () => {
       .get("https://restcountries.com/v3.1/all")
       .then(res => {
         setCountries(res.data)
+        setIsLoading(false)
       })
   }, [])
 
