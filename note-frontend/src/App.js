5cf0e3ede0d1590841efa832818b427dee230ffe
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-10 13:39:23 +0800

    synced with course material

--- note-frontend/src/App.js
+++ note-frontend/src/App.js
@@ -1,32 +1,8 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
-import noteService from './services/notes';
-
-const Notification = ({ message }) => {
-	if (message === null) {
-		return null
-	}
-
-	return (
-		<div className='error'>
-			{message}
-		</div>
-	)
-}
-
-const Footer = () => {
-	const footerStyle = {
-		color: 'green',
-		fontStyle: 'italic',
-		fontSize: 16
-	}
-	return (
-		<div style={footerStyle}>
-			<br />
-			<em>Note app, Department of Computer Science, University of Helsinki 2022</em>
-		</div>
-	)
-}
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+import noteService from './services/notes'
 
 const App = () => {
 	const [notes, setNotes] = useState([])
@@ -34,23 +10,43 @@ const App = () => {
 	const [showAll, setShowAll] = useState(true)
 	const [errorMessage, setErrorMessage] = useState(null)
 
-
 	useEffect(() => {
 		noteService
 			.getAll()
-			.then(initNotes => {
-				setNotes(initNotes)
+			.then(initialNotes => {
+				setNotes(initialNotes)
 			})
 	}, [])
 
+	const addNote = (event) => {
+		event.preventDefault()
+		const noteObject = {
+			content: newNote,
+			important: Math.random() > 0.5,
+		}
+
+		noteService
+			.create(noteObject)
+			.then(returnedNote => {
+				setNotes(notes.concat(returnedNote))
+				setNewNote('')
+			})
+	}
+
+	const handleNoteChange = (event) => {
+		setNewNote(event.target.value)
+	}
+
+	const notesToShow = showAll
+		? notes
+		: notes.filter(note => note.important)
+
 	const toggleImportanceOf = id => {
-		
 		const note = notes.find(n => n.id === id)
 		const changedNote = { ...note, important: !note.important }
 
 		noteService
-			.update(id, changedNote)
-			.then(returnedNote => {
+			.update(id, changedNote).then(returnedNote => {
 				setNotes(notes.map(note => note.id !== id ? note : returnedNote))
 			})
 			.catch(error => {
@@ -64,51 +60,33 @@ const App = () => {
 			})
 	}
 
-	const addNote = (event) => {
-		event.preventDefault();
-		const noteObject = {
-			content: newNote,
-			important: Math.random() < 0.5,
-			id: notes.length + 1,
-		};
-		noteService
-			.create(noteObject)
-			.then(returnedNote => {
-				setNotes(notes.concat(returnedNote))
-				setNewNote('')
-			})
-	};
-
-	const handleNoteChange = (event) => {
-		console.log(event.target.value);
-		setNewNote(event.target.value);
-	};
-	const toggleShowAll = () => {
-		setShowAll(!showAll);
-	}
-
-	const notesToShow = showAll ? notes : notes.filter((note) => note.important);
-	
 	return (
 		<div>
 			<h1>Notes</h1>
 			<Notification message={errorMessage} />
 			<div>
-				<button onClick={toggleShowAll}>show {showAll ? "important" : "all"}</button>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all'}
+				</button>
 			</div>
 			<ul>
-				{notesToShow.map((note) => (
-					<Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}
-					/>
-				))}
+				<ul>
+					{notesToShow.map(note =>
+						<Note
+							key={note.id}
+							note={note}
+							toggleImportance={() => toggleImportanceOf(note.id)}
+						/>
+					)}
+				</ul>
 			</ul>
 			<form onSubmit={addNote}>
 				<input value={newNote} onChange={handleNoteChange} />
-				<button type='submit'>save</button>
+				<button type="submit">save</button>
 			</form>
 			<Footer />
 		</div>
-	);
-};
+	)
+}
 
-export default App;
+export default App
