d0c79bd80bf5445d0ffbc5cc6fb2241ad62298c2
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-12 17:51:30 +0800

    4.2 tested & stable release

---
+++ part4/app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config');
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+console.log('app running')
+
+module.exports = app
+
+
