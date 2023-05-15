e412461b87b81bb78428586750bfe6303160df23
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-15 13:11:09 +0800

    FSO 4b testing using promises stable

--- note-backend/controllers/notes.js
+++ note-backend/controllers/notes.js
@@ -1,10 +1,17 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+// Promises + then
+// notesRouter.get('/', (request, response) => {
+//   Note.find({}).then(notes => {
+//     response.json(notes)
+//   })
+// })
+
+// async/await
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -29,7 +36,7 @@ notesRouter.post('/', (request, response, next) => {
 
   note.save()
     .then(savedNote => {
-      response.json(savedNote)
+      response.status(201).json(savedNote)
     })
     .catch(error => next(error))
 })
