080933653a1a28e1ad848c45987f3be4208e5bb8
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-22 14:36:41 +0800

    5.23 stable

--- part5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -59,30 +59,31 @@ describe('Blog app', function () {
       describe('When a blog is created', function () {
         beforeEach(function () {
           cy.contains('new blog').click()
-          cy.get('#title-input').type('this is the title')
+          cy.get('#title-input').type('this is blog One')
           cy.get('#author-input').type('May Cho')
-          cy.get('#url-input').type('www.example.com')
+          cy.get('#url-input').type('www.example1.com')
+          cy.contains('create').click()
+          cy.contains('new blog').click()
+          cy.get('#title-input').type('this is blog Two')
+          cy.get('#author-input').type('May Cho')
+          cy.get('#url-input').type('www.example2.com')
           cy.contains('create').click()
         })
 
         it('allows users to like a blog', function () {
-          cy.contains('this is the title')
-          cy.contains('May Cho')
           cy.contains('view').click()
           cy.contains('like').click()
           cy.contains('likes 1')
         })
 
         it('allows the user who created a blog to delete it', function () {
-          cy.contains('this is the title')
-          cy.contains('May Cho')
           cy.contains('view').click()
           cy.contains('remove').click()
           cy.get('#main').should('not.contain', 'this is the title')
         })
 
         it('only shows the delete button for the creator of the blog', function () {
-          cy.contains('view').click()
+          cy.get('.blog').eq(0).contains('view').click()
           cy.contains('remove')
           cy.contains('logout').click()
           cy.get('#username').type('wenma')
@@ -90,6 +91,14 @@ describe('Blog app', function () {
           cy.get('#login-button').click()
           cy.contains('view').click()
           cy.get('.blog').should('not.contain', 'remove')
+        })
+        
+        it('shows blogs in a descending order of the likes they have', function() {
+          cy.get('.blog').eq(0).contains('view').click()// Blog One at the top
+          cy.get('.blog').eq(1).contains('view').click()// Blog Two at the second
+          cy.get('.blog').eq(1).contains('like').click()// Like Blog Two
+          cy.get('.blog').eq(0).contains('this is blog Two')// Blog Two becomes the top
+          cy.get('.blog').eq(1).contains('this is blog One')// Blog One becomes the second
         })
       })
 
