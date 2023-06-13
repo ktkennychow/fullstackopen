5286d8cacca73d66570d12279285fadaffa5229c
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-06-13 20:12:49 +0800

    9.11 stable

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,16 +1,18 @@
-export type Gender = "male" | "female" | "other"
+export type Gender = 'male' | 'female' | 'other'
 
 export interface DiagnoseEntry {
-  code: string;
-  name: string;
-  latin?: string;
+  code: string
+  name: string
+  latin?: string
 }
 
 export interface PatientEntry {
-  id: string;
-  name: string;
-  dateOfBirth: string;
-  ssn: string;
-  gender: Gender;
-  occupation: string;
+  id: string
+  name: string
+  dateOfBirth: string
+  ssn: string
+  gender: Gender
+  occupation: string
 }
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>
