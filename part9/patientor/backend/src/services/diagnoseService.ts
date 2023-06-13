ef7b278487b3951a25327eff10839b0343170d1e
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-06-13 19:11:39 +0800

    9.10 stable

---
+++ part9/patientor/backend/src/services/diagnoseService.ts
@@ -0,0 +1,16 @@
+import diagnoses from '../../data/diagnoses'
+
+import { DiagnoseEntry } from '../types'
+
+const getEntries = (): DiagnoseEntry[] => {
+  return diagnoses
+}
+
+const addDiagnose = () => {
+  return null
+}
+
+export default {
+  getEntries,
+  addDiagnose
+}
