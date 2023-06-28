cdadfbc7d5dac3839654f31d0c0bc6a84777d9aa
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-06-28 17:39:39 -0400

    10.2 stable: project setup done

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>What's up RN</Text>
+      <Text>What&apos;s up RN</Text>
       <StatusBar style="auto" />
     </View>
   );
