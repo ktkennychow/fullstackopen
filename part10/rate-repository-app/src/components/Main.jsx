d01717b35c9a1eb47d57a7009db2a0853ee293da
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-06-29 17:08:33 -0400

    10.5 stable: Styled information

--- part10/rate-repository-app/src/components/Main.jsx
+++ part10/rate-repository-app/src/components/Main.jsx
@@ -1,13 +1,12 @@
-import Constants from 'expo-constants'
 import { StyleSheet, View } from 'react-native'
 import RepositoryList from './RepositoryList'
-import Text from './Text'
 import AppBar from './AppBar'
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: '#e1e4e8',
   },
 })
 
--- part10/rate-repository-app/src/components/RepositoryItem.jsx
+++ part10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,17 +1,122 @@
-import { View } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import Text from './Text'
+import theme from '../theme'
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  top: {
+    flexDirection: 'row',
+  },
+  infoContainer: {
+    flexDirection: 'column',
+    marginHorizontal: 20,
+    flexShrink: 1,
+  },
+  info: {
+    paddingBottom: 10,
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: 20,
+  },
+  badges: {
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+  tag: {
+    padding: 10,
+    alignSelf: 'start',
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: theme.colors.primary,
+    overflow: 'hidden',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+})
 
-const Item = ({props}) => {
-  return(
-    <View>
-      <Text>Full name: {props.item.fullName}</Text>
-      <Text>Description: {props.item.description}</Text>
-      <Text>Language: {props.item.language}</Text>
-      <Text>Stars: {props.item.stargazersCount}</Text>
-      <Text>Forks: {props.item.forksCount}</Text>
-      <Text>Reviews: {props.item.reviewCount}</Text>
-      <Text>Rating: {props.item.ratingAverage}</Text>
+const Item = ({ props }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Image
+          style={styles.tinyLogo}
+          source={{ url: props.item.ownerAvatarUrl }}
+        />
+        <View style={styles.infoContainer}>
+          <Text
+            style={styles.info}
+            fontWeight='bold'
+            fontSize='subheading'>
+            {props.item.fullName}
+          </Text>
+          <Text style={styles.info}>{props.item.description}</Text>
+          <Text
+            style={styles.tag}
+            color='textWhite'>
+            {props.item.language}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.bottom}>
+        <View style={styles.badges}>
+          {props.item.stargazersCount > 999 ? (
+            <Text
+              style={{ alignSelf: 'center' }}
+              fontWeight='bold'>
+              {(props.item.stargazersCount / 1000).toFixed(1)}k
+            </Text>
+          ) : (
+            <Text
+              style={{ alignSelf: 'center' }}
+              fontWeight='bold'>
+              {props.item.stargazersCount}
+            </Text>
+          )}
+          <Text style={{ alignSelf: 'center' }}>Stars</Text>
+        </View>
+        <View style={styles.badges}>
+          {props.item.stargazersCount > 999 ? (
+            <Text
+              style={{ alignSelf: 'center' }}
+              fontWeight='bold'>
+              {(props.item.forksCount / 1000).toFixed(1)}k
+            </Text>
+          ) : (
+            <Text
+              style={{ alignSelf: 'center' }}
+              fontWeight='bold'>
+              {props.item.forksCount}
+            </Text>
+          )}
+          <Text style={{ alignSelf: 'center' }}>Forks</Text>
+        </View>
+        <View style={styles.badges}>
+          <Text
+            style={{ alignSelf: 'center' }}
+            fontWeight='bold'>
+            {props.item.reviewCount}
+          </Text>
+          <Text style={{ alignSelf: 'center' }}>Reviews</Text>
+        </View>
+        <View style={styles.badges}>
+          <Text
+            style={{ alignSelf: 'center' }}
+            fontWeight='bold'>
+            {props.item.ratingAverage}
+          </Text>
+          <Text style={{ alignSelf: 'center' }}>Rating</Text>
+        </View>
+      </View>
     </View>
   )
 }
